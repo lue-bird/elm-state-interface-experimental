66e9112046e95588ab5156c2403dfa4675d25c84
lue-bird <lue.the.bird@gmail.com>
2025-04-02 11:52:54 +0200

    make skipping events sound less alarming

--- src/Node.elm
+++ src/Node.elm
@@ -1218,19 +1218,16 @@ programUpdate appConfig event state =
         JsEventFailedToDecode jsonError ->
             ( state
             , let
-                notifyOfBugInterface : InterfaceSingle never_
-                notifyOfBugInterface =
-                    ([ "bug: js event failed to decode: "
-                     , jsonError |> Json.Decode.errorToString
-                     , ". Please open an issue on github.com/lue-bird/elm-state-interface-experimental\n"
-                     ]
-                        |> String.concat
-                    )
-                        |> StandardErrWrite
+                notifyOfSkippedEventInterface : InterfaceSingle never_
+                notifyOfSkippedEventInterface =
+                    StandardErrWrite
+                        ("js event skipped because: "
+                            ++ (jsonError |> Json.Decode.errorToString)
+                        )
               in
               idAndDiffToJson
-                (notifyOfBugInterface |> interfaceSingleToStructuredId |> StructuredId.toString)
-                (notifyOfBugInterface |> Add)
+                (notifyOfSkippedEventInterface |> interfaceSingleToStructuredId |> StructuredId.toString)
+                (notifyOfSkippedEventInterface |> Add)
                 |> appConfig.ports.toJs
             )
 
